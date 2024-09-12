051c0a305ae016c3d66777e7bdbcb3fa5f33513f
iffydopsqueen <omaleweks@gmail.com>
2024-09-12 21:36:20 +0000

    Added internet gateway, NAT gateway and elastic IP for the ngw

--- staging/us-east-2/main.tf
+++ staging/us-east-2/main.tf
@@ -7,3 +7,36 @@ resource "aws_vpc" "staging" {
     Environment = "staging"
   }
 }
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "staging" {
+  vpc_id = aws_vpc.staging.id
+
+  tags = {
+    Name = "staging-igw"
+  }
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "staging" {
+  allocation_id = aws_eip.natgw_eip.id
+  subnet_id = aws_subnet.staging_pub_1.id
+
+  tags = {
+    Name = "staging-ngw"
+  }
+
+  # To ensure proper ordering, add an explicit dependency
+  depends_on = [ aws_internet_gateway.staging ]
+}
+
+# Create an Elastic IP for the NAT Gateway
+resource "aws_eip" "natgw_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "ngw-eip"
+  }
+
+  depends_on = [ aws_internet_gateway.staging ]
+}
