4a395b88b0cfd35cecf98d3270f67c6e7909b5e3
iffydopsqueen <omaleweks@gmail.com>
2024-09-18 19:29:15 +0000

    Added the auto scaling group resource to my infra

--- staging/us-east-2/asg.tf
+++ staging/us-east-2/asg.tf
@@ -16,9 +16,30 @@ resource "aws_launch_template" "staging_lt" {
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "${var.env}-lt"
+      Name = "${var.env}-asg-web-server"
     }
   }
 
   user_data = filebase64("web.sh")
+}
+
+###################### ASG ######################
+
+resource "aws_autoscaling_group" "staging_asg" {
+  name = "${var.env}-asg"
+  vpc_zone_identifier = [ aws_subnet.staging_prv_1.id, aws_subnet.staging_prv_2.id]
+  desired_capacity = 2
+  max_size = 3
+  min_size = 1
+
+  launch_template {
+    id = aws_launch_template.staging_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key = "name"
+    value = "${var.env}-asg-web-server"
+    propagate_at_launch = true
+  }
 }
