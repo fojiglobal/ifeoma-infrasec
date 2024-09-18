e05c11de7d1cdba9972dcf852c2ba6d3e90c3987
iffydopsqueen <omaleweks@gmail.com>
2024-09-18 18:53:43 +0000

    Added a data block to get my AMI information

--- staging/us-east-2/data.tf
+++ staging/us-east-2/data.tf
@@ -1,3 +1,17 @@
 data "aws_route53_zone" "route53_cloudwithify" {
   name = "cloudwithify.online."
 }
+
+# data "aws_ami" "nginx_web_ami" {
+#   most_recent = true
+#   owners = [ "self" ]
+
+#   filter {
+#     name = "name"
+#     values = [ "nginx-web-*" ]
+#   }
+# }
+
+# output "my-nginx-ami" {
+#   value = data.aws_ami.nginx_web_ami.id
+# }
