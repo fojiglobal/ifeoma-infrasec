03ffc129f929eab9845da83f81030da244c1ca35
iffydopsqueen <omaleweks@gmail.com>
2024-10-09 21:41:48 +0000

    Added the VPC output for the module

--- staging/us-east-2/vpc.tf
+++ staging/us-east-2/vpc.tf
@@ -5,3 +5,8 @@ module "staging" {
   vpc_cidr = local.vpc_cidr_block
   env = local.env
 }
+
+# Output exported from the outputs.tf in the /modules folder
+output "vpc_id" {
+  value = module.staging.vpc_id
+}
