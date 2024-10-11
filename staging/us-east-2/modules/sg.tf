6ac5d5bbc65c53c832e1c695d0299eb65fc12f57
iffydopsqueen <omaleweks@gmail.com>
2024-10-11 22:46:19 +0000

    Resolved SG ingress rule for CIDR

--- staging/us-east-2/modules/sg.tf
+++ staging/us-east-2/modules/sg.tf
@@ -45,7 +45,7 @@ resource "aws_vpc_security_group_ingress_rule" "pub_sg_ingress" {
   for_each = var.public_sg_ingress
 
   security_group_id            = aws_security_group.public.id
-  referenced_security_group_id = each.value["src"]
+  cidr_ipv4                    = each.value["src"]
   from_port                    = each.value["from_port"]
   to_port                      = each.value["to_port"]
   ip_protocol                  = each.value["ip_protocol"]
@@ -77,7 +77,7 @@ resource "aws_vpc_security_group_ingress_rule" "bastion_sg_ingress" {
   for_each = var.bastion_sg_ingress
 
   security_group_id            = aws_security_group.bastion.id
-  referenced_security_group_id = each.value["src"]
+  cidr_ipv4                    = each.value["src"]
   from_port                    = each.value["from_port"]
   to_port                      = each.value["to_port"]
   ip_protocol                  = each.value["ip_protocol"]
