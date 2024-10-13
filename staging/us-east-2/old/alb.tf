4b1ee2ad20820b42268589d5ae94f234e1bd3e83
iffydopsqueen <omaleweks@gmail.com>
2024-10-13 22:32:08 +0000

    Resolved snyk high severity vulnerability - ALB does not drop invalid headers

--- staging/us-east-2/old/alb.tf
+++ staging/us-east-2/old/alb.tf
@@ -16,11 +16,12 @@ resource "aws_lb_target_group" "staging_tg" {
 
 resource "aws_lb" "staging_alb" {
   name               = "${var.env}-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.pub_sg.id]
   subnets            = [aws_subnet.staging_pub_1.id, aws_subnet.staging_pub_2.id]
-
+  internal           = false
+  security_groups    = [aws_security_group.pub_sg.id]
+  load_balancer_type = "application"
+  drop_invalid_header_fields = true
+  
   tags = {
     Name        = "${var.env}-alb"
     Environment = var.env
