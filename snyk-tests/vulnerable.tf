fd27d17f3c1fdce499c1f3849d371bab1d3c2d35
iffydopsqueen <omaleweks@gmail.com>
2024-10-16 02:13:11 +0000

    Resolved snyk high severity vulnerability - S3 block public policy control is disabled

--- snyk-tests/vulnerable.tf
+++ snyk-tests/vulnerable.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
   block_public_acls       = true
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
