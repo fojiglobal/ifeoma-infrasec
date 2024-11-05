3a1ac909a51512125c58f3336e5b0ac59f7017f0
iffydopsqueen <omaleweks@gmail.com>
2024-11-05 23:45:54 +0000

    Linked our accounts to their respective organizational units for proper structure

--- aws-org/aws-org.tf
+++ aws-org/aws-org.tf
@@ -12,10 +12,12 @@ resource "aws_organizations_organization" "org" {
 resource "aws_organizations_account" "cs2-sandbox" {
   name  = "cs2-sandbox"
   email = "alearning58+cs2sandbox@gmail.com"
+  parent_id = aws_organizations_organizational_unit.cs2-sandbox.id
 }
 
 # AWS Organization Account - 'CS2-Secops' 
 resource "aws_organizations_account" "cs2-secops" {
   name  = "cs2-secops"
   email = "alearning58+cs2secops@gmail.com"
+  parent_id = aws_organizations_organizational_unit.cs2-secops.id
 }
