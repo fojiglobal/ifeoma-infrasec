eab7709f278cfbfb84dc77b514aecec5ed523553
iffydopsqueen <omaleweks@gmail.com>
2025-01-29 23:22:27 +0000

    Python Basics

--- python/week1/basics.py
+++ python/week1/basics.py
@@ -24,7 +24,7 @@
 # age = current_year - birth_year
 # print("I am" , age , "years old.")
 
-#################################### Input from User ####################################
+# ----------------------------------------------------------------------------------
 
 # birth_year = int(input("Enter your birth year: "))
 # current_year = int(input("Enter the current year: "))
